6fbbb510d8b4d4842a6bfa366349f019273295fa
kien <nhukien24112003@gmail.com>
2023-12-24 16:59:50 +0700

    Xử lý đăng nhập với servlet

--- Drill_Sell/src/main/java/vn/edu/hcmuaf/bean/User.java
+++ Drill_Sell/src/main/java/vn/edu/hcmuaf/bean/User.java
@@ -3,18 +3,28 @@ package vn.edu.hcmuaf.bean;
 public class
 User {
     private int id;
-    private String username;
-    private String fullname;
-    private String password;
+    private String fullname,
+            address,
+            phone,
+            email,
+            username,
+            password,
+            sex,
+            yearOfBirth;
 
     public User() {
     }
 
-    public User(int id, String username, String fullname, String password) {
+    public User(int id, String fullname, String address, String phone, String email, String username, String password, String sex, String yearOfBirth) {
         this.id = id;
-        this.username = username;
         this.fullname = fullname;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.username = username;
         this.password = password;
+        this.sex = sex;
+        this.yearOfBirth = yearOfBirth;
     }
 
     public int getId() {
@@ -25,20 +35,44 @@ User {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getFullname() {
         return fullname;
     }
 
     public void setFullname(String fullname) {
         this.fullname = fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -49,13 +83,34 @@ User {
         this.password = password;
     }
 
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(String yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
                 ", fullname='" + fullname + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                ", yearOfBirth='" + yearOfBirth + '\'' +
                 '}';
     }
 }
