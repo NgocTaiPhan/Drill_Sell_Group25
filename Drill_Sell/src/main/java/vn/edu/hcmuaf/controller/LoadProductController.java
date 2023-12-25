39a2aa53df6fe676c1bb1131e194dbf82c561233
Phan Ngọc Tài <21130164@st.hcmuaf.edu.vn>
2023-12-25 16:05:28 +0700

    Chỉnh sửa và thêm các class để load sản phẩm

---
+++ Drill_Sell/src/main/java/vn/edu/hcmuaf/controller/LoadProductController.java
@@ -0,0 +1,52 @@
+package vn.edu.hcmuaf.controller;
+
+import vn.edu.hcmuaf.bean.Products;
+import vn.edu.hcmuaf.serice.ProductService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "LoadProductController", value = {"/battery_drill", "/load-products"})
+public class LoadProductController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("UTF-8");
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String contextPath = request.getContextPath();
+        switch (request.getServletPath()) {
+            case "/battery_drill":
+                List<Products> productsList2 = ProductService.getAll();
+                List<Products> productsList3 = ProductService.getAll();
+                List<List<Products>> loadAllProducts1 = new ArrayList<>();
+                loadAllProducts1.add(productsList2);
+                loadAllProducts1.add(productsList3);
+                request.setAttribute("loadProductInHome", loadAllProducts1);
+                request.getRequestDispatcher("battery_drill.jsp").forward(request, response);
+                break;
+            case "/load-products":
+                List<Products> productsList = ProductService.getAll();
+                List<Products> productsList1 = ProductService.getAll();
+                List<List<Products>> loadAllProducts = new ArrayList<>();
+                loadAllProducts.add(productsList);
+                loadAllProducts.add(productsList1);
+                request.setAttribute("loadProductInHome", loadAllProducts);
+                request.getRequestDispatcher("home.jsp").forward(request, response);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
--- Drill_Sell/src/main/java/vn/edu/hcmuaf/serice/UserService.java
+++ Drill_Sell/src/main/java/vn/edu/hcmuaf/serice/UserService.java
@@ -10,14 +10,9 @@ import java.util.stream.Collectors;
 
 public class UserService {
 
-    private static Map<String, String> tmpUser = new HashMap<>();
-
-    static {
-        tmpUser.put("admin@gmail.com", "admin");
-        tmpUser.put("ThuyTrang@gmail.com", "123");
-    }
-
     private static UserService instance;
+
+    private UserService() {}
 
     public static UserService getInstance() {
         if (instance == null) instance = new UserService();
@@ -32,9 +27,7 @@ public class UserService {
         });
         if (users.size() != 1) return null;
         User u = users.get(0);
-        if (username.equals(u.getUsername().equals(u.getPassword()))) {
-            return u;
-        }
-        return null;
+        return (password.equals(u.getPassword())) ? u : null;
     }
 }
+
